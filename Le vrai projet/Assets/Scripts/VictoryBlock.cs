bf6cbf5a85b20c3f26b3e23bceaadc9413d8a5b5
Adrien VITTI <adrien.vitti@gmail.com>
2014-03-08 02:49:43 -0500

    Detection de victoire OK

---
+++ Le vrai projet/Assets/Scripts/VictoryBlock.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VictoryBlock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		rigidbody2D.gravityScale = 0;
+		collider2D.isTrigger = true;
+		// Nothing !
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Nothing !
+	}
+
+	void OnTriggerEnter2D (Collider2D coll) {
+		if (coll.gameObject.tag == "Player") {
+			Debug.Log("Victory !");
+			//TODO: Victory stuff here.
+		}
+	}
+
+}
