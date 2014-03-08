5cf6fb17f737a0af9afcee88fff50c00158b952d
tommylavoie <tom.patate@hotmail.com>
2014-03-07 20:43:37 -0500

    Creation des blocs

---
+++ Le vrai projet/Assets/Blocs/Scripts/ElectricBloc.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElectricBox : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D collider)
+	{
+		if(collider.gameObject == GameObject.Find ("Player"))
+		{
+			collider.gameObject.SendMessage("Death");
+		}
+	}
+}
