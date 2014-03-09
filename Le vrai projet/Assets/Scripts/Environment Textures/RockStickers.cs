8362d0b3e1c1395a776187af92b1876268b29f5d
Gabriel Côté-Moisescu <gabrielcotem@gmail.com>
2014-03-09 13:13:53 -0400

    Fini les textures

---
+++ Le vrai projet/Assets/Scripts/Environment Textures/RockStickers.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockStickers : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		int randomTextureID = Random.Range (0, 3);
+
+		GetComponent<SpriteRenderer>().sprite = Resources.Load ("gemSticker/gemSticker" + randomTextureID, typeof(Sprite)) as Sprite;
+
+		transform.localScale = new Vector3 (transform.localScale.x * (96f / 100f), transform.localScale.y * (96f / 100f), transform.localScale.z);
+	
+	}
+}
